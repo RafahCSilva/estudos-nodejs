a9eb7459734190e7d3700d5b55e091dfc4e18959
RafahCSilva <rafahcsilva@gmail.com>
2022-03-29 01:57:58 -0300

    Mutation para alterar

--- graphql/mutation/resolvers/Mutation.js
+++ graphql/mutation/resolvers/Mutation.js
@@ -18,4 +18,17 @@ module.exports = {
     if (index < 0) return null
     return DB.usuarios.splice(index, 1)[0] || null
   },
+
+  alterarUsuario (_, args) {
+    const index = DB.usuarios.findIndex(u => u.id === args.id)
+    if (index < 0) return null
+    return DB.usuarios[index] = {
+      ...DB.usuarios[index],
+      ...args,
+    }
+    // return DB.usuarios.splice(index, 1, {
+    //   ...DB.usuarios[index],
+    //   ...args,
+    // })[0]
+  },
 }
