6fbd9f390fae408ddf8c3e029ab3c86d95413996
RafahCSilva <rafahcsilva@gmail.com>
2022-03-28 23:33:48 -0300

    Criando um tipo

--- graphql/schema-queries/index.js
+++ graphql/schema-queries/index.js
@@ -6,10 +6,20 @@ const typeDefs = gql`
   # scalar é os tipos, pois somente tem os basicos: Int, Float, String, Boolean e ID
   scalar Date
 
+  # Uma nova query
+  type Usuario {
+    id: ID!
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+  }
+
   # ponto de entrada da sua API!
-  type Query{
+  type Query {
     ola: String
     horaAtual: Date
+    usuarioLogado: Usuario
   }
 `
 
@@ -24,6 +34,19 @@ const resolvers = {
       // return (new Date()).toLocaleString()
       return new Date()
     },
+    usuarioLogado () {
+      return {
+        id: 1,
+        nome: 'Ana da Web',
+        email: 'anadaweb@email.com',
+        idade: 23,
+        salario_real: 1234.56,
+      }
+    },
+  },
+  Usuario: {
+    // um resolver na query `Usuario{}` para mapear um prop
+    salario (usuario) { return usuario.salario_real},
   },
 }
 
