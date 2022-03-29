5772d40e4d9f9fc8b22320a89a5dac7a5d2a5d80
RafahCSilva <rafahcsilva@gmail.com>
2022-03-29 01:45:06 -0300

    Mutation para excluir

--- graphql/mutation/resolvers/Mutation.js
+++ graphql/mutation/resolvers/Mutation.js
@@ -12,4 +12,10 @@ module.exports = {
     DB.usuarios.push(novo)
     return novo
   },
+
+  excluirUsuario (_, { id }) {
+    const index = DB.usuarios.findIndex(u => u.id === id)
+    if (index < 0) return null
+    return DB.usuarios.splice(index, 1)[0] || null
+  },
 }
