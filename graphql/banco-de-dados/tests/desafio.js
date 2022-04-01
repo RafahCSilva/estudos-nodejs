2488ceefe2cf6a771571412b23967a1ea7abde22
RafahCSilva <rafahcsilva@gmail.com>
2022-04-01 00:55:37 -0300

    Desafio pt2

--- graphql/banco-de-dados/tests/desafio.js
+++ graphql/banco-de-dados/tests/desafio.js
@@ -1,32 +1,61 @@
 const db = require('../config/db')
 
 async function salvarUsuario (nome, email, senha) {
-  return db('usuarios')
-    .insert({ nome, email, senha })
+  let usuario = await db('usuarios').where({ email }).first()
+  let usuario_id
+
+  if (!usuario) { // CREATE
+    let [id] = await db('usuarios')
+      .insert({ nome, email, senha })
+    usuario_id = id
+  } else { // UPDATE
+    let id = usuario.id
+    await db('usuarios')
+      .where({ id })
+      .update({ nome, email, senha })
+    usuario_id = id
+  }
+  usuario = await db('usuarios').where({ id: usuario_id }).first()
+  return usuario
 }
 
 async function salvarPerfil (nome, rotulo) {
-  return db('perfis')
-    .insert({ nome, rotulo })
+  let perfil = await db('perfis').where({ nome }).first()
+
+  if (!perfil) { // CREATE
+    [nome] = await db('perfis')
+      .insert({ nome, rotulo })
+  } else { // UPDATE
+    await db('perfis')
+      .where({ nome })
+      .update({ nome, rotulo })
+  }
+  perfil = await db('perfis').where({ nome }).first()
+  return perfil
 }
 
 async function adicionarPerfis (usuario, ...perfis) {
-  return db('usuarios_perfis')
-    .insert(perfis.map(p => {
-      return {
-        usuario_id: usuario[0],
-        perfil_id: p,
-      }
-    }))
+  await db('usuarios_perfis')
+    .where({ usuario_id: usuario.id })
+    .delete()
+
+  return await db('usuarios_perfis')
+    .insert(perfis
+      .map(p => {
+        return {
+          usuario_id: usuario.id,
+          perfil_id: p.id,
+        }
+      }))
 }
 
 async function executar () {
-  // TRUNCATE
-  await db('usuarios_perfis').delete()
-  await db('perfis').delete()
-  await db('usuarios').delete()
+  // // TRUNCATE
+  // await db('usuarios_perfis').delete()
+  // await db('perfis').delete()
+  // await db('usuarios').delete()
 
-  const usuario = await salvarUsuario('Ana', 'ana@empresa.com.br' + Math.random(), '123456')
+  const usuario = await salvarUsuario('Ana', 'ana@empresa.com.br', '123456')
   const perfilA = await salvarPerfil('rh', 'Pessoal')
   const perfilB = await salvarPerfil('fin', 'Financeiro')
 
