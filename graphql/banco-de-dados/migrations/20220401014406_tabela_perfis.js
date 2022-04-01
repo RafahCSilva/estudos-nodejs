5895beeb4b637d15a2e49984ea9a3a9feacde131
RafahCSilva <rafahcsilva@gmail.com>
2022-03-31 23:20:03 -0300

    Seed depois de createTable

--- graphql/banco-de-dados/migrations/20220401014406_tabela_perfis.js
+++ graphql/banco-de-dados/migrations/20220401014406_tabela_perfis.js
@@ -7,6 +7,12 @@ exports.up = function (knex) {
     table.increments('id').primary()
     table.string('nome').notNullable().unique()
     table.string('rotulo').notNullable()
+  }).then(() => {
+    return knex('perfis').insert([
+      { nome: 'comum', rotulo: 'Comum' },
+      { nome: 'admin', rotulo: 'Administrador' },
+      { nome: 'master', rotulo: 'Master' },
+    ])
   })
 }
 
