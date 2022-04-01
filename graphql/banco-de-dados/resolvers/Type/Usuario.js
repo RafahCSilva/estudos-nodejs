d7e60c53226261852c768669f77edcc73fbcffe7
RafahCSilva <rafahcsilva@gmail.com>
2022-04-01 02:25:23 -0300

    Desafio GraphQL: Query with Join

--- graphql/banco-de-dados/resolvers/Type/Usuario.js
+++ graphql/banco-de-dados/resolvers/Type/Usuario.js
@@ -2,6 +2,10 @@ const db = require('../../config/db')
 
 module.exports = {
   async perfis (usuario) {
-    // TODO: Implementar (Assunto novo!)
+    return db
+      .select('perfis.id', 'perfis.nome', 'perfis.rotulo')
+      .from('perfis')
+      .join('usuarios_perfis', 'usuarios_perfis.perfil_id', '=', 'perfis.id')
+      .where({ 'usuarios_perfis.usuario_id': usuario.id })
   },
 }
